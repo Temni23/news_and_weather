02505ab878cb27a762e36812d79cf0f2dd994e6c
Artem Klimov <temni23@yandex.ru>
2023-09-21 12:44:00 +0700

    База данных настроена под том докера.

--- news_and_weather/settings.py
+++ news_and_weather/settings.py
@@ -69,7 +69,7 @@ POSTGRES_SETTINGS = {
 
 SQLITE_SETTINGS = {
     'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3',
+    'NAME': BASE_DIR / 'data_base/db.sqlite3',
 }
 
 DATABASES = {
