e6bbfe956b7176178ecfd3e59f9d0160a47a158d
Artem Klimov <temni23@yandex.ru>
2023-09-22 16:25:23 +0700

    Создан эндпоинт и функция для генерации файла с данными о погоде

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import PublicationViewSet, PlaceViewSet, WeatherReportViewSet
+from api.views import PublicationViewSet, PlaceViewSet, WeatherReportViewSet, \
+    download_weather_data
 
 app_name = 'api'
 
@@ -13,4 +14,8 @@ router_v1.register('weather', WeatherReportViewSet, basename='weather')
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
     path('auth/', include('djoser.urls.authtoken')),
-    path('', include('djoser.urls')), ]
+    path('', include('djoser.urls')),
+    path('download_weather_data/', download_weather_data,
+         name='download_weather_data')
+    # http://127.0.0.1:8000/api/download_weather_data/?date=2023-09-22&place_id=1
+]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,13 @@
+import xlsxwriter
 from rest_framework import viewsets
+from django.http import HttpResponse
 
 from api.serializers import (PublicationSerializer, PlaceSerializer,
                              WeatherReportSerializer)
 from news.models import Publication
 from places.models import Place, WeatherReport
+
+from datetime import datetime
 
 
 class PublicationViewSet(viewsets.ModelViewSet):
@@ -22,3 +26,39 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
 class WeatherReportViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = WeatherReport.objects.all()
     serializer_class = WeatherReportSerializer
+
+
+def download_weather_data(request):
+    """По GET запросу пользователя генерирует эксель файл с данными о погоде"""
+    if request.method == 'GET':
+        date = request.GET.get('date')
+        place_id = request.GET.get('place_id')
+        date = datetime.strptime(date, '%Y-%m-%d')
+        weather_data = WeatherReport.objects.filter(report_time__date=date,
+                                                    place_id=place_id)
+        response = HttpResponse(
+            content_type='application/vnd.openxmlformats-officedocument.'
+                         'spreadsheetml.sheet')
+        response[
+            'Content-Disposition'] = (f'attachment; '
+                                      f'filename=weather_report_{date}.xlsx')
+
+        workbook = xlsxwriter.Workbook(response, {'in_memory': True})
+        worksheet = workbook.add_worksheet()
+        row = 0
+        col = 0
+        # Заголовки столбцов
+        worksheet.write(row, col, 'Температура (°C)')
+        worksheet.write(row, col + 1, 'Влажность (%)')
+        worksheet.write(row, col + 2, 'Атмосферное давление (мм рт. ст.)')
+        worksheet.write(row, col + 3, 'Место')
+        for data in weather_data:
+            row += 1
+            worksheet.write(row, col, data.temperature)
+            worksheet.write(row, col + 1, data.humidity)
+            worksheet.write(row, col + 2, data.pressure)
+            worksheet.write(row, col + 3, data.place.name)
+        workbook.close()
+        return response
+
+
